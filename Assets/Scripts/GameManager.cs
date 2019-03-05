00e6383bbefc771ca18b2acd784bdb6232e0bc82
DamianP <dploszaj@primebitgames.com>
2019-03-05 20:26:05 +0100

    Jumping

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private static GameManager instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+				instance = GameObject.FindObjectOfType<GameManager>();
+			return instance;
+		}
+	}
+	public float groundLevel;
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,15 +12,17 @@ public enum PlayerState
 public class PlayerMovement : MonoBehaviour
 {
 	public PlayerState playerState;
-	[SerializeField] float jumpHeight;
+	[SerializeField] float jumpHeight, jumpSpeed, maxTimeInAir;
 
-	Vector3 playerPosition;
+	float timeInAir, jumpPressedTime;
+	bool jumpHolding, doubleJumped;
+	Vector3 playerPositionJumping, playerPositionFalling;
 
 
 	private void Start()
 	{
 		playerState = PlayerState.Running;
-		playerPosition = transform.position;
+		playerPositionJumping = transform.position;
 	}
 
 	public void Jump()
@@ -37,14 +39,83 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if (playerState == PlayerState.Jumping)
 		{
-
-		}
-		else if (playerState == PlayerState.Falling)
-		{
-			if (transform.position.y < 0)
+			timeInAir += Time.deltaTime;
+			if (transform.position.y > playerPositionJumping.y + jumpHeight)
+			{
+				playerState = PlayerState.Falling;
+			}
+			else if (timeInAir > jumpPressedTime)
+			{
+				playerState = PlayerState.Falling;
+			}
+			else if (transform.position.y > playerPositionJumping.y + jumpHeight)
 			{
 
 			}
+			else
+			{
+				//float jump = jumpHeight - (transform.position.y - playerPositionJumping.y) / (jumpHeight);
+
+
+
+				float jump = 1 - (timeInAir / jumpPressedTime);
+
+				Debug.Log("jmp speed: " + jump);
+				transform.position = new Vector3(transform.position.x, transform.position.y + jump * jumpSpeed * Time.deltaTime);
+			}
+		}
+		else if (playerState == PlayerState.Falling)
+		{
+			if (transform.position.y < GameManager.Instance.groundLevel)
+			{
+				transform.position = new Vector3(transform.position.x, GameManager.Instance.groundLevel);
+				playerState = PlayerState.Running;
+			}
+			else
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y - jumpSpeed * Time.deltaTime);
+			}
+		}
+
+		JumpController();
+
+		//Debug.Log(jumpPressedTime + "	" + timeInAir);
+	}
+
+	void JumpController()
+	{
+		//jump
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (playerState == PlayerState.Running)
+			{
+				jumpPressedTime = 0.1f;
+				timeInAir = 0;
+				doubleJumped = false;
+				playerPositionJumping = transform.position;
+				playerState = PlayerState.Jumping;
+				jumpHolding = true;
+			}
+			// double jump
+			else if (!doubleJumped)
+			{
+				jumpPressedTime = 0.2f;
+				timeInAir = 0;
+				doubleJumped = true;
+				playerPositionJumping = transform.position;
+				playerState = PlayerState.Jumping;
+				jumpHolding = true;
+			}
+		}
+		// button holding
+		if (Input.GetButton("Jump") && playerState == PlayerState.Jumping && jumpHolding && jumpPressedTime < maxTimeInAir)
+		{
+			jumpPressedTime += Time.deltaTime;
+		}
+		// button up
+		else if (Input.GetButtonUp("Jump"))
+		{
+			jumpHolding = false;
 		}
 	}
 }
